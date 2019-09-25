2bc0f005e30522340dec2f455312b9d922cd4638
MElder96 <55810529+MElder96@users.noreply.github.com>
2019-09-25 15:40:25 -0500

    project-upload
    Moved Unity project into the repo

---
+++ bombs away/Assets/Scripts/Collectible.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HandleCollected();
+    }
+    void HandleCollected()
+    {
+        CollectibleManager.Instance.HandleBombCollected();
+        Destroy(gameObject);
+    }
+}
---
+++ bombs away/Assets/Scripts/CollectibleManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CollectibleManager : MonoBehaviour
+{
+    public static CollectibleManager Instance; 
+
+    private int _totalBombs;
+    private int _collectedBombs;
+
+    public TextMeshProUGUI bombCount;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    private void Start()
+    {
+        _totalBombs = FindObjectsOfType<Collectible>().Length;
+        DisplayBombCount();
+    }
+    void DisplayBombCount()
+    {
+        bombCount.SetText(_collectedBombs + " / " + _totalBombs);
+    }
+
+    public void HandleBombCollected()
+    {
+        _collectedBombs++;
+        //_collectedBombs = _collectedBombs + 1;
+        //another way of adding 
+        DisplayBombCount();
+        if (_collectedBombs == _totalBombs)
+        {
+           // bombCount.setText("you win!");
+        }
+    }
+}
