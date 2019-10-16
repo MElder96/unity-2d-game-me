a0080c29c9c3f6212b0a81f9d64de5e6f8831bc3
MElder96 <55810529+MElder96@users.noreply.github.com>
2019-10-16 14:47:27 -0500

    character assets error
    not sure what is going on

--- bombs away/Assets/Scripts/CrateSpawner.cs
+++ bombs away/Assets/Scripts/CrateSpawner.cs
@@ -8,16 +8,5 @@ public class CrateSpawner : MonoBehaviour
 
     public int amountOfCratesToSpawn;
 
-    private void Start()
-    {
-        //declare it an int setting it at 0, as long as i is less than keep wokring, add number by 1
-        for (int i = 0; i < amountOfCratesToSpawn; i ++)
-        {
-<<<<<<< Updated upstream
-            //object to copy, position of create spawner location, rotation current, parent = crateSpawner
-=======
->>>>>>> Stashed changes
-            Instantiate(cratePrefab, transform.localPosition, Quaternion.identity, this.transform);
-        }
-    }
+
 }
